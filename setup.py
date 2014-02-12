b4b5cd8162165cad924e737afc076444806051ad
Rob Harrigan <rharrigan@50onred.com>
2014-02-12 10:08:52 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,20 +2,22 @@
 from setuptools import setup
 import os
 
+__version__ = '0.1.2'
+
 def read(*paths):
     """Build a file path from *paths* and return the contents."""
     with open(os.path.join(*paths), 'r') as f:
         return f.read()
 
 setup(name='ftpretty',
-      version='0.1.1',
+      version=__version__,
       description='Pretty FTP wrapper',
       long_description=read('README.rst'),
       license='MIT',
       author='Rob Harrigan',
       author_email='harrigan.rob@gmail.com',
       url='https://github.com/codebynumbers/ftpretty/',
-      download_url='https://github.com/codebynumbers/ftpretty/tarball/0.1.1',
+      download_url='https://github.com/codebynumbers/ftpretty/tarball/%s' % __version__,
       py_modules=['ftpretty'],
      )
 
