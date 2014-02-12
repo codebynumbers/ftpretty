14f212048ab86b1a341e032cb120928e97bbcdfe
Rob Harrigan <rharrigan@50onred.com>
2014-02-11 22:12:05 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 from distutils.core import setup
 
 setup(name='ftpretty',
-      version='0.1.0',
+      version='0.1.1',
       description='Pretty FTP wrapper',
       author='Rob Harrigan',
       author_email='harrigan.rob@gmail.com',
       url='https://github.com/codebynumbers/ftpretty',
-      download_url='https://github.com/codebynumbers/ftpretty/tarball/0.1.0',
+      download_url='https://github.com/codebynumbers/ftpretty/tarball/0.1.1',
       py_modules=['ftpretty'],
      )
 
