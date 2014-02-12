66a2f0e9a9431867ae3bb5e83a5453ca072cff9b
Rob Harrigan <rharrigan@50onred.com>
2014-02-11 21:39:20 -0500

    Add download_url to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='ftpretty',
       author='Rob Harrigan',
       author_email='harrigan.rob@gmail.com',
       url='https://github.com/codebynumbers/ftpretty',
+      download_url='https://github.com/codebynumbers/ftpretty/tarball/0.1.0',
       py_modules=['ftpretty'],
      )
 
