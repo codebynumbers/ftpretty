8d20dc00c233fbb459a99a0f2ea4bbd47944a2e6
Rob Harrigan <rharrigan@50onred.com>
2014-02-11 21:37:22 -0500

    Add setup file and update ignore for sdist stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ftpretty',
+      version='0.1.0',
+      description='Pretty FTP wrapper',
+      author='Rob Harrigan',
+      author_email='harrigan.rob@gmail.com',
+      url='https://github.com/codebynumbers/ftpretty',
+      py_modules=['ftpretty'],
+     )
+
