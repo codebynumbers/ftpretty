b133b4bbd1d6104c0d30b1914be1ac140fe59aae
Antonio Jordan <antonio.jordan@jordan.dco-intranet.lan>
2018-07-19 18:51:29 +0000

    renaming assertEquals to assertEqual per deprecation warning on Travis

--- tests/test_ftpretty.py
+++ tests/test_ftpretty.py
@@ -14,43 +14,43 @@ class FtprettyTestCase(TestCase):
 
     def test_cd(self):
         self.pretty.cd('photos/nature/mountains')
-        self.assertEquals(self.pretty.pwd(), 'photos/nature/mountains')
+        self.assertEqual(self.pretty.pwd(), 'photos/nature/mountains')
         self.pretty._set_exists(False)
         self.assertRaises(Exception, self.pretty.cd('blah'))
 
     def test_cd_up(self):
         self.pretty.cd('photos/nature/mountains')
         self.pretty.cd('../..')
-        self.assertEquals(self.pretty.pwd(), 'photos')
+        self.assertEqual(self.pretty.pwd(), 'photos')
 
     def test_descend(self):
         self.pretty._set_exists(False)
         self.pretty.descend('photos/nature', True)
         self.pretty._set_exists(True)
         self.pretty.cd('mountains')
-        self.assertEquals(self.pretty.pwd(), 'photos/nature/mountains')
+        self.assertEqual(self.pretty.pwd(), 'photos/nature/mountains')
 
     def test_list(self):
         self.mock_ftp._set_files(['a.txt', 'b.txt'])
-        self.assertEquals(len(self.pretty.list()), 2)
+        self.assertEqual(len(self.pretty.list()), 2)
 
     def test_list_relative_paths(self):
         self.mock_ftp._set_files(['.', '..', 'a.txt'])
-        self.assertEquals(len(self.pretty.list(remove_relative_paths=True)), 1)
+        self.assertEqual(len(self.pretty.list(remove_relative_paths=True)), 1)
 
     def test_put_contents(self):
         put_contents = token_bytes(128)
         size = self.pretty.put(None, 'foobar.txt', put_contents)
-        self.assertEquals(size, len(BytesIO(put_contents).getvalue()))
+        self.assertEqual(size, len(BytesIO(put_contents).getvalue()))
 
     def test_put_file(self):
         with open('foobar.txt', 'rb') as file_:
             size = self.pretty.put(file_, 'foobar.txt')
-        self.assertEquals(size, path.getsize('foobar.txt'))
+        self.assertEqual(size, path.getsize('foobar.txt'))
 
     def test_put_filename(self):
         size = self.pretty.put('foobar.txt', 'foobar.txt')
-        self.assertEquals(size, path.getsize('foobar.txt'))
+        self.assertEqual(size, path.getsize('foobar.txt'))
         remove('foobar.txt')
 
     def test_upload_tree(self):
@@ -62,7 +62,7 @@ class FtprettyTestCase(TestCase):
         with open('tree/bar/baz.txt', 'wb') as file:
             file.write(contents)
         tree = self.pretty.upload_tree('tree', '/tree')
-        self.assertEquals(tree, '/tree')
+        self.assertEqual(tree, '/tree')
         remove('tree/bar/baz.txt')
         remove('tree/foo.txt')
         rmdir('tree/bar')
@@ -76,7 +76,7 @@ class FtprettyTestCase(TestCase):
             file.write(get_contents)
         self.mock_ftp._set_contents(get_contents)
         contents = self.pretty.get('remote_file.txt')
-        self.assertEquals(contents, get_contents)
+        self.assertEqual(contents, get_contents)
         remove('remote_file.txt')
 
     def test_get(self):
@@ -93,7 +93,7 @@ class FtprettyTestCase(TestCase):
         self.pretty.get('remote_file.txt', 'local_copy.txt')
         self.assertTrue(path.isfile('local_copy.txt'))
         with open('local_copy.txt', 'rb') as file:
-            self.assertEquals(file.read(), get_contents)
+            self.assertEqual(file.read(), get_contents)
         remove('local_copy.txt')
         remove('remote_file.txt')
 
@@ -112,7 +112,7 @@ class FtprettyTestCase(TestCase):
             self.pretty.get('remote_file.txt', outfile)
         self.assertTrue(path.isfile('local_copy.txt'))
         with open('local_copy.txt', 'rb') as file:
-            self.assertEquals(file.read(), get_contents)
+            self.assertEqual(file.read(), get_contents)
         remove('local_copy.txt')
         remove('remote_file.txt')
 
@@ -131,35 +131,35 @@ class FtprettyTestCase(TestCase):
                 "-rw-rw-r-- 1 rharrigan nobody 2085 Feb 21 13:27 multi word name.png\n" +
                 "-rw-rw-r-- 1 rharrigan wheel  195 Feb 20 2013 README.txt\n")
         files = self.pretty.list(extra=True)
-        self.assertEquals(len(files), 3)
+        self.assertEqual(len(files), 3)
 
         current_year = int(datetime.now().strftime('%Y'))
-        self.assertEquals(files[1]['name'], 'multi word name.png')
-        self.assertEquals(files[1]['datetime'], datetime(current_year, 2, 21, 13, 27, 0))
+        self.assertEqual(files[1]['name'], 'multi word name.png')
+        self.assertEqual(files[1]['datetime'], datetime(current_year, 2, 21, 13, 27, 0))
 
-        self.assertEquals(files[2]['datetime'], datetime(2013, 2, 20, 0, 0, 0))
-        self.assertEquals(files[2]['size'], 195)
-        self.assertEquals(files[2]['name'], 'README.txt')
-        self.assertEquals(files[2]['owner'], 'rharrigan')
-        self.assertEquals(files[2]['group'], 'wheel')
+        self.assertEqual(files[2]['datetime'], datetime(2013, 2, 20, 0, 0, 0))
+        self.assertEqual(files[2]['size'], 195)
+        self.assertEqual(files[2]['name'], 'README.txt')
+        self.assertEqual(files[2]['owner'], 'rharrigan')
+        self.assertEqual(files[2]['group'], 'wheel')
 
     def test_dir_parse_patched_regex(self):
         self.mock_ftp._set_dirlist("-rw-rw-r-- 1 rharrigan read-only   47 Feb 20 11:39 Cool.txt\n" +
                 "-rw-rw-r-- 1 rharrigan nobody 2085 Feb 21 13:27 multi word name.png\n" +
                 "-rw-rw-r-- 1 rharrigan dodgy-group-name  195 Feb 20 2013 README.txt\n")
         files = self.pretty.list(extra=True)
-        self.assertEquals(len(files), 3)
+        self.assertEqual(len(files), 3)
 
         current_year = int(datetime.now().strftime('%Y'))
-        self.assertEquals(files[0]['group'], 'read-only')
-        self.assertEquals(files[1]['name'], 'multi word name.png')
-        self.assertEquals(files[1]['datetime'], datetime(current_year, 2, 21, 13, 27, 0))
+        self.assertEqual(files[0]['group'], 'read-only')
+        self.assertEqual(files[1]['name'], 'multi word name.png')
+        self.assertEqual(files[1]['datetime'], datetime(current_year, 2, 21, 13, 27, 0))
 
-        self.assertEquals(files[2]['datetime'], datetime(2013, 2, 20, 0, 0, 0))
-        self.assertEquals(files[2]['size'], 195)
-        self.assertEquals(files[2]['name'], 'README.txt')
-        self.assertEquals(files[2]['owner'], 'rharrigan')
-        self.assertEquals(files[2]['group'], 'dodgy-group-name')
+        self.assertEqual(files[2]['datetime'], datetime(2013, 2, 20, 0, 0, 0))
+        self.assertEqual(files[2]['size'], 195)
+        self.assertEqual(files[2]['name'], 'README.txt')
+        self.assertEqual(files[2]['owner'], 'rharrigan')
+        self.assertEqual(files[2]['group'], 'dodgy-group-name')
 
     def test_fallthrough(self):
         self.assertTrue(self.pretty.sendcmd('hello'), 'hello')
